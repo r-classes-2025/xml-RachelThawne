5cc2daa21648874c9d1b170a51994e7abed0692a
RachelThawne <123176294+RachelThawne@users.noreply.github.com>
2025-11-14 01:12:46 +0300

    Дай бог последний коммит

--- assignment.R
+++ assignment.R
@@ -19,82 +19,61 @@ unzip("letters.zip")
 my_xmls <- list.files("letters/", full.names = TRUE)
 
 # Сначала напишите код для первого письма в датасете, чтобы потренироваться. 
-
 test_xml <- my_xmls[1]
-doc <- read_xml(test_xml)
+doc <- read_xml (test_xml)
 ns <- xml_ns(doc)
 
-  
+
 # дата письма
 date <- doc |>
-  xml_find_first("//correspAction[@type='sending']/date") |>
-  xml_attr("when") |>
-  trimws()
+  xml_find_first("//d1:correspAction[@type = 'sending']//d1:date", ns) |>
+  xml_attr("when")
+
 
 # адресат письма
 corresp <- doc |>
-  xml_find_first("//correspAction[@type='receiving']") |>
+  xml_find_first("//d1:correspAction[@type = 'receiving']//d1:persName", ns) |>
   xml_text() |>
   trimws()
 
+
 # том 
-vol <- doc |>
-  xml_find_first("//biblScope[@unit='vol']") |>
+vol <- doc |> 
+  xml_find_first("//d1:biblScope[@unit = 'vol']", ns) |>
   xml_text() |>
   trimws()
 
 
 ## Когда все получится, оберните свое решение в функцию read_letter().
 
-
 read_letter <- function(xml_path) {
+  doc <- read_xml(xml_path)
+  ns <- xml_ns(doc)
   
-  date_node <- xml_find_first(doc, "//correspAction[@type='sending']/date")
-  date <- ifelse(length(date_node) == 0, 
-                 NA_character_, 
-                 xml_attr(date_node, "when") |>
-                   trimws())
+  date <- doc |>
+    xml_find_first("//d1:correspAction[@type = 'sending']//d1:date", ns) |>
+    xml_attr("when")
   
-  corresp_node <- xml_find_first(doc, ".//correspAction[@type='receiving']")
-  if (length(corresp_node) == 0) {
-    corresp_node <- xml_find_first(doc, ".//correspAction[contains(@type, 'receiving')]")
-  }
+  corresp <- doc |>
+    xml_find_first("//d1:correspAction[@type = 'receiving']//d1:persName", ns) |>
+    xml_text() |>
+    trimws()
   
-  corresp <- NA_character_
-  if (length(corresp_node) > 0) {
-    # Пробуем разные способы извлечения текста адресата
-    corresp_text <- xml_text(corresp_node) %>% trimws()
-    if (corresp_text != "") {
-      corresp <- corresp_text
-    } else {
-      # Если текста нет напрямую, ищем в дочерних элементах
-      pers_name <- xml_find_first(corresp_node, ".//persName")
-      if (length(pers_name) > 0) {
-        corresp <- xml_text(pers_name) %>% trimws()
-      }
-    }
-  }
-  
-  vol_node <- xml_find_first(doc, "//biblScope[@unit='vol']")
-  vol <- ifelse(length(vol_node) == 0, 
-                NA_character_, 
-                xml_text(vol_node) |>
-                  trimws())
-
+  vol <- doc |>
+    xml_find_first("//d1:biblScope[@unit = 'vol']", ns) |>
+    xml_text() |>
+    trimws()
   
   # записываем в тиббл
   res <- tibble(
-    vol = vol,
     date = date,
-    corresp = corresp
-   )
-
+    corresp = corresp,
+    vol = vol
+  )
+  
   return(res)
 }
 
-
 # Прочтите все письма в один тиббл при помощи map_dfr(). 
 letters_tbl <- map_dfr(my_xmls, read_letter)
-letters_tbl
-
 
